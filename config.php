f773fce3541b465654af442b8c468c332e506f0e
Agil Fajar Dwi Prasetyo <69827604+Agellls@users.noreply.github.com>
2022-10-26 12:00:00 +0700

    Revert "add CRUD PHP file"

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "perpustakaan";
-$connect = mysqli_connect($host, $user, $password, $database) or die("koneksi gagal");
--- session.php
+++
@@ -1,9 +0,0 @@
-<?php
-// memulai session
-session_start();
-// inisialisasi variabel session
-$_SESSION['username'] = 'admin';
-// akses variable session
-echo "Session Id : $_SESSION[user_id]";
-echo "<br>";
-echo "Session Name : $_SESSION[username]";
